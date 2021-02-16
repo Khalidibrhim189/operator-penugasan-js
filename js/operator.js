ef56e282de8676bd3f379c740f93cb71cb727233
khalidjambi <khalid.jambi@gmail.comm>
2021-02-16 09:56:10 +0700

    operatorpenugasan

---
+++ js/operator.js
@@ -0,0 +1,16 @@
+// Operator Matemmatika
+
+
+var namaDepan = 'Khalid';
+var namaBelakang = ' Ibrahim';
+
+var gabunganNama = namaDepan + namaBelakang;
+
+
+var usia = 21;
+
+
+document.getElementById('name').innerHTML = gabunganNama;
+document.getElementById('usia').innerHTML = usia;
+
+console.log(typeof(usia));
